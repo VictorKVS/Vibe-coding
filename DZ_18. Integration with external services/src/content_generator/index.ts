91f9334a89c76ede9d3c19c1c31511a249ecf56a
Виктор <92217904+VictorKVS@users.noreply.github.com>
2026-09-18 21:40:04 +0300

    refactor(dz18): export generator runtime modules

--- DZ_18. Integration with external services/src/content_generator/index.ts
+++ DZ_18. Integration with external services/src/content_generator/index.ts
@@ -1,4 +1,6 @@
 export * from "./briefs/handoff";
 export * from "./personas/types";
+export * from "./personas/reference-personas";
 export * from "./scenes/types";
+export * from "./storyboard/planner";
 export * from "./providers/types";
