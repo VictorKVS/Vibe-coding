cbfa30c54aa867229841703748eae3b17186f436
Виктор <92217904+VictorKVS@users.noreply.github.com>
2026-09-19 22:50:12 +0300

    refactor(dz18): export Image Model Zoo

--- DZ_18. Integration with external services/src/father/index.ts
+++ DZ_18. Integration with external services/src/father/index.ts
@@ -4,3 +4,5 @@ export * from "./prompt_engineering/types";
 export * from "./prompt_engineering/registry";
 export * from "./model_zoo/types";
 export * from "./model_zoo/registry";
+export * from "./image_zoo/types";
+export * from "./image_zoo/registry";
