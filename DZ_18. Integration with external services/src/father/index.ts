05bd0c2dac573fcaad76d69416c5f5d99258889d
Виктор <92217904+VictorKVS@users.noreply.github.com>
2026-09-19 22:44:11 +0300

    refactor(dz18): export Model Zoo registry

--- DZ_18. Integration with external services/src/father/index.ts
+++ DZ_18. Integration with external services/src/father/index.ts
@@ -2,3 +2,5 @@ export * from "./agent_zoo/types";
 export * from "./agent_zoo/registry";
 export * from "./prompt_engineering/types";
 export * from "./prompt_engineering/registry";
+export * from "./model_zoo/types";
+export * from "./model_zoo/registry";
